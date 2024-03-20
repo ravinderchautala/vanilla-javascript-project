346a0592bb04e9a836028a4ffc86ce9340e4c311
ravinderchautala <ryvinder@gmail.com>
2024-03-20 17:26:37 +0530

    counter project completed

--- project-1/js/hex.js
+++ project-1/js/hex.js
@@ -6,7 +6,7 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function () {
     let code = '#';
     for (let i = 0; i <= 5; i++) {
-        let number = Math.floor(Math.random() * 16);
+        let number = Math.floor(Math.random() * hex.length);
         code = code + hex[number];
     }
     color.textContent = code;
--- project-2/js/app.js
+++ project-2/js/app.js
@@ -1,61 +1,51 @@
-const value = document.getElementById('value');
-const btns = document.querySelectorAll('.btn');
+const number = document.querySelector('.number');
 let count = 0;
 
+const btns = document.querySelectorAll('.btn');
+
+
 // method-1
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const list = e.currentTarget.classList;
-        if (list.contains("decrease")) {
-            count--;
-        } else if (list.contains("increase")) {
-            count++;
-        } else {
-            count = 0;
-        }
-        if (count > 0) {
-            value.style.color = 'blue';
-        }
-        if (count < 0) {
-            value.style.color = 'red';
-        }
-        if (count === 0) {
-            value.style.color = 'black';
-        }
-        value.textContent = count;
-    });
-});
+
+// btns[0].addEventListener('click', function () {
+//     count--;
+//     number.textContent = count;
+//     number.style.color = 'red';
+// });
+// btns[1].addEventListener('click', function () {
+//     count = 0;
+//     number.textContent = count;
+//     number.style.color = 'black';
+// });
+// btns[2].addEventListener('click', function () {
+//     count++;
+//     number.textContent = count;
+//     number.style.color = 'blue';
+// });
+
 
 // method-2
 
-const decrease = document.querySelector('.decrease');
-const reset = document.querySelector('.reset');
-const increase = document.querySelector('.increase');
-
-// decrease.addEventListener('click', function () {
-//     count--;
-//     value.textContent = count;
-//     if (count > 0) {
-//         value.style.color = 'blue';
-//     } else if (count < 0) {
-//         value.style.color = 'red';
-//     } else {
-//         value.style.color = 'black';
-//     }
-// })
-// reset.addEventListener('click', function () {
-//     count = 0;
-//     value.textContent = count;
-//     value.style.color = 'black';
-// })
-// increase.addEventListener('click', function () {
-//     count++;
-//     value.textContent = count;
-//     if (count > 0) {
-//         value.style.color = 'blue';
-//     } else if (count < 0) {
-//         value.style.color = 'red';
-//     } else {
-//         value.style.color = 'black';
-//     }
-// })
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count === 0) {
+            number.style.color = "black";
+        }
+        else if (count > 0) {
+            number.style.color = "blue";
+        }
+        else {
+            number.style.color = "red";
+        }
+        number.textContent = count;
+    })
+})
